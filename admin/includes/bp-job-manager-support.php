bb849b5edda8af6848db30fc5bb0f599ca77fcd9
Pallavi Gupta <pallavi@wbcomdesigns.com>
2017-08-31 01:22:49 +0530

    optional plugin links in support section

--- admin/includes/bp-job-manager-support.php
+++ admin/includes/bp-job-manager-support.php
@@ -16,6 +16,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 								<li><a href="https://wpjobmanager.com/" target="_blank">WordPress Job Manager</a> - <i><?php _e( 'Free Available', BPJM_TEXT_DOMAIN )?></i></li>
 								<li><a href="https://wpjobmanager.com/add-ons/applications/" target="_blank">WordPress Job Manager - Applications</a> - <i><?php _e( 'Paid Addon', BPJM_TEXT_DOMAIN )?></i></li>
 								<li><a href="https://wpjobmanager.com/add-ons/resume-manager/" target="_blank">WordPress Job Manager - Resume Manager</a> - <i><?php _e( 'Paid Addon', BPJM_TEXT_DOMAIN )?></i></li>
+								<li><a href="https://wpjobmanager.com/add-ons/bookmarks/" target="_blank">WordPress Job Manager - Bookmarks</a> - <i><?php _e( 'Paid Addon - Optional for this plugin', BPJM_TEXT_DOMAIN )?></i></li>
+								<li><a href="https://wpjobmanager.com/add-ons/job-alerts/" target="_blank">WordPress Job Manager - Alerts</a> - <i><?php _e( 'Paid Addon - Optional for this plugin', BPJM_TEXT_DOMAIN )?></i></li>
 							</ol>
 						</p>
 					</div>
