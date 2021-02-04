713037e9ce6a7c3dcb35d538052f8c678e392a96
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2021-02-04 18:10:21 +0530

    (#101) PHPCS fixes (admin)

--- admin/wbcom/templates/wbcom-support-page.php
+++ admin/wbcom/templates/wbcom-support-page.php
@@ -1,53 +1,53 @@
 <div class="wrap">
 	<?php echo do_shortcode( '[wbcom_admin_setting_header]' ); ?>
 	<h4 class="wbcom-plugin-heading"><?php esc_html_e( 'Support', 'bp-job-manager' ); ?></h4>
-    <div id="wbcom_admin_content_support" class="wbcom_admin_tab_content">
-        <strong><?php esc_html_e( 'Have any question or issue ?', 'bp-job-manager' ); ?></strong>
-        <p>
+	<div id="wbcom_admin_content_support" class="wbcom_admin_tab_content">
+		<strong><?php esc_html_e( 'Have any question or issue ?', 'bp-job-manager' ); ?></strong>
+		<p>
 			<?php esc_html_e( 'Our team is here to help you out anytime. If you have any idea about how we can improve, you can share it on our helpdesk along with your site access for faster response. There are two ways to stay in touch with us :', 'bp-job-manager' ); ?></p>
 
-        <ul id="wbcom_support_list" class="wbcom_boxes_list wp-clearfix">
+		<ul id="wbcom_support_list" class="wbcom_boxes_list wp-clearfix">
 
-            <li class="wbcom_single_box wp-clearfix">
-                <div class="wbcom_single_left">
-                    <div class="wbcom_single_icon_wrapper">
-                        <i class="fas fa-2x fa-tags"></i>
-                    </div>
-                </div>
-                <div class="wbcom_single_right">
-                    <div class="wbcom_single_inner">
-                        <h4><?php esc_html_e( 'Support Tickets', 'bp-job-manager' ); ?></h4>
-                        <p><?php esc_html_e( 'You can open a support ticket on our helpdesk. If you have purchased one of our products, we will get back to you within next 24 hours.', 'bp-job-manager' ); ?></p>
-                        <div class="text-right">
-                            <a href="https://support.wbcomdesigns.com/portal/newticket" class="wb_btn wb_btn_default" target="_blank">
-                                <i class="fas fa-ticket-alt"></i>
+			<li class="wbcom_single_box wp-clearfix">
+				<div class="wbcom_single_left">
+					<div class="wbcom_single_icon_wrapper">
+						<i class="fas fa-2x fa-tags"></i>
+					</div>
+				</div>
+				<div class="wbcom_single_right">
+					<div class="wbcom_single_inner">
+						<h4><?php esc_html_e( 'Support Tickets', 'bp-job-manager' ); ?></h4>
+						<p><?php esc_html_e( 'You can open a support ticket on our helpdesk. If you have purchased one of our products, we will get back to you within next 24 hours.', 'bp-job-manager' ); ?></p>
+						<div class="text-right">
+							<a href="https://support.wbcomdesigns.com/portal/newticket" class="wb_btn wb_btn_default" target="_blank">
+								<i class="fas fa-ticket-alt"></i>
 								<?php esc_html_e( 'Open a ticket', 'bp-job-manager' ); ?>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </li>
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
 
-            <li class="wbcom_single_box wp-clearfix">
-                <div class="wbcom_single_left">
-                    <div class="wbcom_single_icon_wrapper">
-                        <i class="fas fa-2x fa-comments"></i>
-                    </div>
-                </div>
-                <div class="wbcom_single_right">
-                    <div class="wbcom_single_inner">
-                        <h4><?php esc_html_e( 'Contact Us', 'bp-job-manager' ); ?></h4>
-                        <p><?php esc_html_e( 'For any other queries, you can contact us via our Contact Us page. We will make sure to get back to you as soon as possible.', 'bp-job-manager' ); ?></p>
-                        <div class="text-right">
-                            <a href="https://wbcomdesigns.com/contact/" class="wb_btn wb_btn_default" target="_blank">
-                                <i class="fas fa-phone-square"></i>
+			<li class="wbcom_single_box wp-clearfix">
+				<div class="wbcom_single_left">
+					<div class="wbcom_single_icon_wrapper">
+						<i class="fas fa-2x fa-comments"></i>
+					</div>
+				</div>
+				<div class="wbcom_single_right">
+					<div class="wbcom_single_inner">
+						<h4><?php esc_html_e( 'Contact Us', 'bp-job-manager' ); ?></h4>
+						<p><?php esc_html_e( 'For any other queries, you can contact us via our Contact Us page. We will make sure to get back to you as soon as possible.', 'bp-job-manager' ); ?></p>
+						<div class="text-right">
+							<a href="https://wbcomdesigns.com/contact/" class="wb_btn wb_btn_default" target="_blank">
+								<i class="fas fa-phone-square"></i>
 								<?php esc_html_e( 'Contact Us', 'bp-job-manager' ); ?>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </li>
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
 
-        </ul>
-    </div>
+		</ul>
+	</div>
 </div>
--- admin/wbcom/templates/wbcom-themes-page.php
+++ admin/wbcom/templates/wbcom-themes-page.php
@@ -11,14 +11,14 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'REIGN Community', 'bp-job-manager' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Multiple Member Directory Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Member Header Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Group Directory Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Group Header Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'BuddyPress Customization Options', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'BuddyPress Extra Widgets', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Inbuilt Social Profile Module', 'bp-job-manager' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Multiple Member Directory Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Multiple Member Header Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Multiple Group Directory Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Multiple Group Header Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'BuddyPress Customization Options', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'BuddyPress Extra Widgets', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Inbuilt Social Profile Module', 'bp-job-manager' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a href="https://wbcomdesigns.com/downloads/reign-buddypress-theme/#pricing-table-leranmate" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'bp-job-manager' ); ?></a>
 							<a target="_blank" href="http://demos.wbcomdesigns.com/reign-demo1/" class="wbcom-button"><?php esc_html_e( 'Preview', 'bp-job-manager' ); ?></a>
@@ -37,14 +37,14 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'Learnmate', 'bp-job-manager' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Add Review to your course', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Course Coming Soon', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Grid And List Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Learndash/LifterLMS Extra Widgets', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Unparalleled Course Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome Learndash/LifterLMS Pages', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Distraction Free Reading', 'bp-job-manager' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Add Review to your course', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Course Coming Soon', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Grid And List Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Learndash/LifterLMS Extra Widgets', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Unparalleled Course Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Awesome Learndash/LifterLMS Pages', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Distraction Free Reading', 'bp-job-manager' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a href="https://wbcomdesigns.com/downloads/learnmate-learndash/#pricing-table-leranmate" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'bp-job-manager' ); ?></a>
 							<a target="_blank" href="https://wpbp.in/learndash/" class="wbcom-button" ><?php esc_html_e( 'Preview', 'bp-job-manager' ); ?></a>
@@ -63,12 +63,12 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'ShopMate Dokan', 'bp-job-manager' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Dokan Extra Widgets', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Unique Single Product Page Customization', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome WooCommerce Pages', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Grid And List Layout', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Automated Mega Menu Support', 'bp-job-manager' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Dokan Extra Widgets', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Unique Single Product Page Customization', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Awesome WooCommerce Pages', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Grid And List Layout', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Automated Mega Menu Support', 'bp-job-manager' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a target="_blank" href="https://wbcomdesigns.com/downloads/storemate-dokan/#pricing-table-leranmate" class="wbcom-button wbcom-purchase"><?php esc_html_e( 'Purchase', 'bp-job-manager' ); ?></a>
 							<a target="_blank" href="https://demos.wbcomdesigns.in/reign-dokan/" class="wbcom-button"><?php esc_html_e( 'Preview', 'bp-job-manager' ); ?></a>
@@ -87,11 +87,11 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'Jobmate Theme', 'bp-job-manager' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Simple live searching and filtering', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Unique Job listing Page', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome Resume list Page', 'bp-job-manager' ); ?></li>
-                            <li><?php esc_html_e( 'Outstanding all job maanger pagesLayout', 'bp-job-manager' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Simple live searching and filtering', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Unique Job listing Page', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Awesome Resume list Page', 'bp-job-manager' ); ?></li>
+							<li><?php esc_html_e( 'Outstanding all job maanger pagesLayout', 'bp-job-manager' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a target="_blank" href="https://wbcomdesigns.com/downloads/jobmate-theme/" class="wbcom-button wbcom-purchase"><?php esc_html_e( 'Purchase', 'bp-job-manager' ); ?></a>
 							<a target="_blank" href="http://demos.wbcomdesigns.in/jobmate-theme/" class="wbcom-button"><?php esc_html_e( 'Preview', 'bp-job-manager' ); ?></a>
