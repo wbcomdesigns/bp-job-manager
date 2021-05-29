18b47270b914aa816be1c27cb0988d4c607802e4
vapvarun <vapvarun@gmail.com>
2021-05-30 02:44:04 +0530

    Update bp-job-manager-welcome-page.php

--- admin/includes/bp-job-manager-welcome-page.php
+++ admin/includes/bp-job-manager-welcome-page.php
@@ -19,7 +19,7 @@ if (!defined('ABSPATH')) {
         <div class="wbcom-welcome-content">
 
             <div class="wbcom-video-link-wrapper">
-                <iframe src="https://player.vimeo.com/video/556674671" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/556674671?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="BuddyPress Job Manager"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
             </div>
 
             <div class="wbcom-welcome-support-info">
